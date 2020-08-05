43330809f21a72c65d0d2cab711ed557e6d968a9
Raman <raman@pop-os.localdomain>
2020-08-05 09:09:51 +0530

    Add empty class for Fashion2Vec

---
+++ fashion_intel/apis/siamese.py
@@ -0,0 +1,19 @@
+from fashion_intel.imports import *
+
+class Fashion2Vec:
+    """ 
+    A class to provide fashion image embeddings
+    """
+
+    def __init__(self):
+        pass
+
+    def generate_embeddings(self, image_ids: List, storage_path: str) -> None:
+        """ This function generates feature embeddings of images
+        Args:
+            image_ids (List): ids of image
+        Return:
+            None
+        """
+        pass
+
