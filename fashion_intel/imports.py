12e64928b40ad8283bf58ec25d92b1a6e6331910
Raman <raman@pop-os.localdomain>
2020-08-05 09:25:50 +0530

    Update imports

--- fashion_intel/imports.py
+++ fashion_intel/imports.py
@@ -1,11 +1,34 @@
-# General imports
+# General libraries
 import os
 import json
 import random
+from ast import literal_eval
+from tqdm.notebook import tqdm as tqdm_notebook
+from tqdm import tqdm
 
 # Machine learning libraries
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 # Typing
 from typing import List
+
+# Torch 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+from torch.autograd import Variable
+
+# Torcvision
+from torchvision import transforms
+import torchvision.models as models
+
+# PyTorch Lightning
+import pytorch_lightning as pl
+
+# Image and visualization libraries
+from PIL import Image
+from matplotlib import pyplot as plt
