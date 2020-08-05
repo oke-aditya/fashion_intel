de4089b47ed3d8683e003ec83e6cf68d5da794a0
Raman <raman@pop-os.localdomain>
2020-08-05 09:03:56 +0530

    Add blueprint for tagger class

---
+++ fashion_intel/apis/tagger.py
@@ -0,0 +1,19 @@
+from fashion_intel.imports import *
+
+class Tagger:
+    """ 
+    A class to provide image tagging 
+    """
+
+    def __init__(self):
+        pass
+
+    def get_tags(self, image_id: int) -> str:
+        """ This function is used to get images for feed
+        Args:
+            image_id: id of image
+        Return:
+            str: the generated description of image
+        """
+        pass
+
