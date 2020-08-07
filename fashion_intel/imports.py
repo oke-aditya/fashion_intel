784650049729ed10837a02dfb1f8b197fe82cf93
ramaneswaran <51799927+ramaneswaran@users.noreply.github.com>
2020-08-07 23:54:35 +0530

    Add the APIs and inference codes (#18)
    * Add feed api
    
    * Add recommender api
    
    * Add tagger api
    
    * Add siamese model files
    
    * Add ranker files
    
    * Add tagger files
    
    * Add imports files
    
    * Add models folder
    
    * Update .gitignore
    
    Co-authored-by: Raman <raman@pop-os.localdomain>

---
+++ fashion_intel/imports.py
@@ -0,0 +1,41 @@
+# General libraries
+import os
+import json
+import random
+from ast import literal_eval
+from tqdm.notebook import tqdm as tqdm_notebook
+from tqdm import tqdm
+import pickle
+
+# Graph libraries
+import networkx as nx
+
+# Machine learning libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+# Typing
+from typing import List
+
+# Torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+from torch.autograd import Variable
+
+# Torcvision
+from torchvision import transforms
+import torchvision.models as models
+
+# PyTorch Lightning
+import pytorch_lightning as pl
+
+# Image and visualization libraries
+from PIL import Image
+from matplotlib import pyplot as plt
+
+# Vectors library
+import faiss
