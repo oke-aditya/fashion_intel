2daea1ff798c8a0df44ce7857d0a4d02625912a7
1UC1F3R616 <kushchoudhary8@gmail.com>
2020-08-08 16:17:30 +0530

    formatted using black

--- scrape_scripts/amazon.py
+++ scrape_scripts/amazon.py
@@ -9,28 +9,88 @@ import os
 import csv
 
 # vars with GLOBAL scope
-bot_query = "https://www.amazon.in/s?k=" # Defualt order, no filters are being applied
-HEADER = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
-PROXY = {"https":"138.0.207.18:58566"}# {"https": "https//124.107.229.210:8080", "http":"http//124.107.229.210:8080"} | Proxy Rotation is absent | pip package will be released sooner
-csv_index = 0 # To keep propering indexing when adding data from different pages
+bot_query = "https://www.amazon.in/s?k="  # Defualt order, no filters are being applied
+HEADER = {
+    "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
+}
+PROXY = {
+    "https": "138.0.207.18:58566"
+}  # {"https": "https//124.107.229.210:8080", "http":"http//124.107.229.210:8080"} | Proxy Rotation is absent | pip package will be released sooner
+csv_index = 0  # To keep propering indexing when adding data from different pages
 
 ## Error handling needed when image sizes mismatch
-def csv_append(user_query='-', item_count='-', name='-', item_page='-', rating='-', review='-', is1='-', is15='-', is2='-', is25='-', is3='-', price='-'):
+def csv_append(
+    user_query="-",
+    item_count="-",
+    name="-",
+    item_page="-",
+    rating="-",
+    review="-",
+    is1="-",
+    is15="-",
+    is2="-",
+    is25="-",
+    is3="-",
+    price="-",
+):
     global is_first
     global csv_index
 
     csv_index += 1
-    item_count = csv_index # Overtake
-    csv_name = user_query.replace('+', '-') + '.csv'
+    item_count = csv_index  # Overtake
+    csv_name = user_query.replace("+", "-") + ".csv"
     if is_first:
-        with open(csv_name, 'w', newline='', encoding="utf-8") as f:
+        with open(csv_name, "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
-            writer.writerow(["SN", "Name", "Link to item", "Rating", "Reviews", "Image Download link 1x", "Image Download link 1.5x", "Image Download link 2x", "Image Download link 2.5x", "Image Download link 3x", "Price"])
-            writer.writerow([item_count, name, item_page, rating, review.replace(',', ''), is1, is15, is2, is25, is3, price])
+            writer.writerow(
+                [
+                    "SN",
+                    "Name",
+                    "Link to item",
+                    "Rating",
+                    "Reviews",
+                    "Image Download link 1x",
+                    "Image Download link 1.5x",
+                    "Image Download link 2x",
+                    "Image Download link 2.5x",
+                    "Image Download link 3x",
+                    "Price",
+                ]
+            )
+            writer.writerow(
+                [
+                    item_count,
+                    name,
+                    item_page,
+                    rating,
+                    review.replace(",", ""),
+                    is1,
+                    is15,
+                    is2,
+                    is25,
+                    is3,
+                    price,
+                ]
+            )
     else:
-        with open(csv_name, 'a+', newline='', encoding="utf-8") as f:
+        with open(csv_name, "a+", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
-            writer.writerow([item_count, name, item_page, rating, review.replace(',', ''), is1, is15, is2, is25, is3, price])
+            writer.writerow(
+                [
+                    item_count,
+                    name,
+                    item_page,
+                    rating,
+                    review.replace(",", ""),
+                    is1,
+                    is15,
+                    is2,
+                    is25,
+                    is3,
+                    price,
+                ]
+            )
+
 
 def slugify(img_name):
     """
@@ -39,14 +99,18 @@ def slugify(img_name):
     :return: slugged and format typed image
     """
 
-    sluged_name =img_name.replace(' ', '-').replace('\"', '').replace('(', ' ').replace(')', ' ')
-    sluged_name = sluged_name.replace('|', ' ').replace('&', ' ').replace(':', ' ') # ASCII Range can be checked instead, More info needed about how will the images \
+    sluged_name = (
+        img_name.replace(" ", "-").replace('"', "").replace("(", " ").replace(")", " ")
+    )
+    sluged_name = (
+        sluged_name.replace("|", " ").replace("&", " ").replace(":", " ")
+    )  # ASCII Range can be checked instead, More info needed about how will the images \
     # be used.
-    
-    return "{}.{}".format(sluged_name, 'jpg')
+
+    return "{}.{}".format(sluged_name, "jpg")
 
 
-def img_download(url, img_name):#img_name):
+def img_download(url, img_name):  # img_name):
     """
     Downloads the image from given url
     :param1: url
@@ -55,10 +119,10 @@ def img_download(url, img_name):#img_name):
     # yes change here
     global csv_index
 
-    #img_name = slugify(img_name).encode('ascii', 'ignore')
+    # img_name = slugify(img_name).encode('ascii', 'ignore')
     img_name = slugify(str(csv_index))
     img = requests.get(url, headers=HEADER)
-    with open(img_name, 'wb') as f: # Put them in folder properly
+    with open(img_name, "wb") as f:  # Put them in folder properly
         f.write(img.content)
 
 
@@ -66,7 +130,7 @@ def scrape_bundle(user_query, page_count):
 
     global is_first
     is_first = True
-    for count in range(1, int(page_count)+1):
+    for count in range(1, int(page_count) + 1):
         scrape_query(user_query, count)
         print(count)
         is_first = False
@@ -83,15 +147,13 @@ def scrape_query(user_query, page_count):
 
     global is_first
 
-    amazon_query = bot_query + user_query + '&' + 'page=' + str(page_count) 
+    amazon_query = bot_query + user_query + "&" + "page=" + str(page_count)
 
-    client = requests.get(amazon_query, headers=HEADER) # proxies=PROXY | gimmeproxy
+    client = requests.get(amazon_query, headers=HEADER)  # proxies=PROXY | gimmeproxy
     page_html = client.text
 
-
     page_soup = soup(page_html, "html.parser")
-    container = page_soup.findAll("div", {"class":"s-result-item"})
-    
+    container = page_soup.findAll("div", {"class": "s-result-item"})
 
     bundled_data = []
 
@@ -101,24 +163,27 @@ def scrape_query(user_query, page_count):
     if len(container) > 0:
         item_count = 0
         for item in container:
-            
+
             item_count += 1
-            item_page = 'https://www.amazon.in' + item.a['href']
+            item_page = "https://www.amazon.in" + item.a["href"]
 
             try:
-                image_srcset =  [(img_link.split(' ')[0], img_link.split(' ')[1]) for img_link in item.a.img['srcset'].split(', ')]
+                image_srcset = [
+                    (img_link.split(" ")[0], img_link.split(" ")[1])
+                    for img_link in item.a.img["srcset"].split(", ")
+                ]
             except:
                 image_srcset = None
-            try: 
-                img_alt = item.img['alt'] # item-name
-            except:
-                img_alt= ''
             try:
-                rating = item.span.find("span", {"class":"a-icon-alt"}).text 
-                review_count = item.span.find("span", {"class":"a-size-base"}).text
+                img_alt = item.img["alt"]  # item-name
             except:
-                rating = '-'
-                review_count = '-'
+                img_alt = ""
+            try:
+                rating = item.span.find("span", {"class": "a-icon-alt"}).text
+                review_count = item.span.find("span", {"class": "a-size-base"}).text
+            except:
+                rating = "-"
+                review_count = "-"
 
             # Availability
             for x in item.span.find_all("span"):
@@ -126,14 +191,13 @@ def scrape_query(user_query, page_count):
                     availability = "Currently unavailable."
             else:
                 availability = "Available"
-            
-            price = item.span.find("span", {"class":"a-price-whole"})
+
+            price = item.span.find("span", {"class": "a-price-whole"})
             if price != None:
                 price = price.text
             else:
-                price = '-'
+                price = "-"
 
-            
             # Downloading most HR image
             try:
                 img_download(url=image_srcset[-1][0], img_name=img_alt)
@@ -141,33 +205,48 @@ def scrape_query(user_query, page_count):
                 print(str(e))
 
             try:
-                is1=image_srcset[0][0]
-                is15=image_srcset[1][0]
-                is2=image_srcset[2][0]
-                is25=image_srcset[3][0]
+                is1 = image_srcset[0][0]
+                is15 = image_srcset[1][0]
+                is2 = image_srcset[2][0]
+                is25 = image_srcset[3][0]
             except:
-                is1='-'
-                is15='-'
-                is2='-'
-                is25='-'
-            try: # Maybe 1 Image we have
-                is3=image_srcset[-1][0]
+                is1 = "-"
+                is15 = "-"
+                is2 = "-"
+                is25 = "-"
+            try:  # Maybe 1 Image we have
+                is3 = image_srcset[-1][0]
             except:
-                is3 = '-'
+                is3 = "-"
 
-            csv_append(user_query=user_query, item_count= item_count, name=img_alt, item_page=item_page, rating=rating, review=review_count, is1=is1, is15=is15, is2=is2, is25=is25, is3=is3, price=price)
+            csv_append(
+                user_query=user_query,
+                item_count=item_count,
+                name=img_alt,
+                item_page=item_page,
+                rating=rating,
+                review=review_count,
+                is1=is1,
+                is15=is15,
+                is2=is2,
+                is25=is25,
+                is3=is3,
+                price=price,
+            )
             is_first = False
     else:
         item = None
         return item
 
+
 if __name__ == "__main__":
 
-    user_query = input("Enter your Product: ").replace(' ', '+') # More filtering is needed | search 'mobile' to get all mobile data scraped
+    user_query = input("Enter your Product: ").replace(
+        " ", "+"
+    )  # More filtering is needed | search 'mobile' to get all mobile data scraped
     page_count = input("Scrape upto how many pages: ")
 
-    if page_count in ['0', ' ', None]:
-        scrape_bundle(user_query, '1')
+    if page_count in ["0", " ", None]:
+        scrape_bundle(user_query, "1")
     else:
         scrape_bundle(user_query, page_count)
-        
